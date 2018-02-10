2685111d2aa9bddbf926da7aa9fe6f42753fb4e2
patrickatcw <pj8392@yahoo.com>
2018-02-10 14:01:57 -0500

    duplicating code problems

--- src/methods.java
+++ src/methods.java
@@ -2,5 +2,32 @@ public class methods {
 
     public static void main(String[] args) {
 
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            //addition to code block, not represented in duplicated code
+            //below, which is problem
+            //result change will not be added below
+            System.out.println("Your final score was " + finalScore);
+
+        }
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        if (gameOver) {
+
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+
+        }
+
     }
 }
