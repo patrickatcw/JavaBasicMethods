4642562fb7b941ee63deb72104aec7bf90ef5a8c
patrickatcw <pj8392@yahoo.com>
2018-02-10 14:07:27 -0500

    created new method calculateScore

--- src/methods.java
+++ src/methods.java
@@ -1,7 +1,8 @@
 public class methods {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {        //main method
 
+        //statements that form main methid
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
@@ -28,6 +29,23 @@ public class methods {
             System.out.println("Your final score was " + finalScore);
 
         }
+    }
+
+    public static void calculateScore() {           //new method
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (gameOver) {
+
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+
+        }
 
     }
+
 }
