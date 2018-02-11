71975c13152da8f05850f41dd0d80978c4b1067a
patrickatcw <pj8392@yahoo.com>
2018-02-10 21:31:19 -0500

    comments added for more explanation of program

--- src/methods.java
+++ src/methods.java
@@ -2,32 +2,32 @@ public class methods {
 
     public static void main(String[] args) {        //main method
 
-        //hardcoding the values, maybe better fro visual and organizing
-        boolean gameOver = true;
+        //hardcoding the values, maybe better for visual and organizing
+        boolean gameOver = true;            //variables
         int score = 800;
         int levelCompleted = 5;
         int bonus = 100;
 
-        calculateScore (gameOver, score, levelCompleted, bonus);
+        calculateScore (gameOver, score, levelCompleted, bonus);  //calling the method
 
-        gameOver = true;
+        gameOver = true;                    //variables
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
-        calculateScore (gameOver, score, levelCompleted, bonus);
+        calculateScore (gameOver, score, levelCompleted, bonus);    //calling the method
 
         }
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {       //new method
-        //parameters variables are defined within quotes, but need actual values above in calculateScore
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {    //new method
+        //parameters variables are defined within quotes, actual values are listed variables above
         //void keyword means do not send any value back
         //but if you do want to return something;so change void to int, to return
         //value of int finalScore
 
-        if (gameOver) {
+        if (gameOver) {                                     //method logic if statement
 
-            int finalScore = score + (levelCompleted * bonus);
+            int finalScore = score + (levelCompleted * bonus);  //incorporating new variable for finalScore
             finalScore += 2000;
             System.out.println("Your final score was " + finalScore);
             return finalScore;          //returns for int change in method from void
